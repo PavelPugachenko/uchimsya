285f1bb34c4d519cf625c0598daa94ad4b626fd0
pavel <pugachenkopavel72@gmail.com>
2025-01-11 00:56:35 +0500

    112

--- library/views.py
+++ library/views.py
@@ -24,12 +24,12 @@ class BookDetailView(DetailView):
     model = Book
     template_name = 'library/book_detail.html'
     context_object_name = 'books'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['author_books_count'] = Book.objects.filter(author=self.object.author).count()
-        return context
-
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['author_books_count'] = Book.objects.filter(author=self.object.author).count()
+    #     return context
+    #
 
 
 class BookUpdateView(UpdateView):
